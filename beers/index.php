564ed953188b4a0a74dd031f9022f216129f316f
fernholz <fernholz@gmail.com>
2013-09-20 16:23:42 -0500

    Fixing header with status bar on ios fullscreen app mode

--- beers/index.php
+++ beers/index.php
@@ -48,6 +48,7 @@ else if($_GET['username'] && $_GET['loggedin']) {
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
         <!-- Bootstrap -->
         <link href="../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
